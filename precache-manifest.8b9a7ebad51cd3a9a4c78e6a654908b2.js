90a8fb25bfc3276e3966b16daae466a39d358573
ilumii <listeven073@gmail.com>
2019-07-30 04:06:44 -0400

    Updates

---
+++ precache-manifest.8b9a7ebad51cd3a9a4c78e6a654908b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec86611194f7eec972fba46d631bdc77",
+    "url": "/bank-app/index.html"
+  },
+  {
+    "revision": "f0419888e2b981e01f66",
+    "url": "/bank-app/static/css/main.c89f6667.chunk.css"
+  },
+  {
+    "revision": "22823eec7b81fe4a64c9",
+    "url": "/bank-app/static/js/2.bcae1836.chunk.js"
+  },
+  {
+    "revision": "f0419888e2b981e01f66",
+    "url": "/bank-app/static/js/main.7bc58c8b.chunk.js"
+  },
+  {
+    "revision": "dfbad1676ffe5cc8a58e",
+    "url": "/bank-app/static/js/runtime~main.fade555e.js"
+  }
+]);
